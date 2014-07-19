e4c45340601e89058601fdf05f84b8c3eaba3506
jamescarl15 <jamescarl15@ymail.com>
2014-07-19 09:18:44 +0800

    Update JamesCarlBlitzCrank.lua

--- JamesCarlBlitzCrank.lua
+++ JamesCarlBlitzCrank.lua
@@ -24,7 +24,7 @@
 --]]
 
 --[[		Auto Update		]]
-local sversion = "1.8"
+local sversion = "1.9"
 local AUTOUPDATE = true --You can set this false if you don't want to autoupdate --
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/jamescarl15/BolStudio/master/JamesCarlBlitzCrank.lua".."?rand="..math.random(1,10000)
